cf8a8eea1069e5307f134959bba4d6e53f93b267
Mukul Yadav <mukulyadav227@gmail.com>
2025-06-08 16:03:53 +0530

    Android Code

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,10 @@
 import React from "react";
 import { fonts } from "@/theme";
-import { arrow_back, CloseTicketIcon } from "@/assets";
+import { arrow_back } from "@/assets";
 import { ms } from "@/utils/scaling-utils";
 import { goBack, navigate } from "@/navigation/RootNavigation";
 import { View, Text, Image, TouchableOpacity, Platform } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
-import { NAVIGATION } from "@/constants";
 import { LinearGradient } from "expo-linear-gradient";
 
 const Header = ({
@@ -17,12 +16,15 @@ const Header = ({
 }) => {
   const insets = useSafeAreaInsets();
   return (
-    <LinearGradient 
-     colors={["#ad5389", "#3c1053"]}
-
-        start={{ x: 0, y: 0 }}
-        end={{ x: 1, y: 1 }}
-        style={[styles.container, { paddingTop:Platform.OS=="ios"? insets.top:insets.top+30 }]}>
+    <LinearGradient
+      colors={["#ad5389", "#3c1053"]}
+      start={{ x: 0, y: 0 }}
+      end={{ x: 1, y: 1 }}
+      style={[
+        styles.container,
+        { paddingTop: Platform.OS == "ios" ? insets.top : insets.top + ms(10) },
+      ]}
+    >
       <TouchableOpacity
         onPress={() => {
           goBackScreen ? navigate(goBackScreen) : goBack();
@@ -50,14 +52,13 @@ const styles = {
     paddingVertical: ms(20),
     paddingHorizontal: ms(16),
     justifyContent: "space-between",
-    paddingTop:Platform.OS=='ios' ?50:80,
+    paddingTop: Platform.OS == "ios" ? 50 : 80,
   },
   icon: {
     width: ms(24),
     height: ms(24),
     resizeMode: "contain",
     tintColor: "white",
-    
   },
   title: {
     color: "white",
